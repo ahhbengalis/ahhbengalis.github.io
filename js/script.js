b72f0f37ef167a9ed3e6becc3b836a0c38f07a50
Onomitra Ghosh <onomitraghosh@gmail.com>
2025-02-21 22:09:24 -0500

    Updated to be movile friendly

--- js/script.js
+++ js/script.js
@@ -1,39 +1,40 @@
 document.addEventListener("DOMContentLoaded", function () {
-  const slides = document.querySelectorAll(".slide");
-  const prev = document.querySelector(".prev");
-  const next = document.querySelector(".next");
-  let currentSlide = 0;
-  const totalSlides = slides.length;
-  let slideInterval = setInterval(showNextSlide, 5000); // 5 seconds
-
-  function showSlide(index) {
-    slides.forEach((slide, idx) => {
-      slide.classList.remove("active");
-      if (idx === index) {
-        slide.classList.add("active");
-      }
+    const slides = document.querySelectorAll(".slide");
+    const prev = document.querySelector(".prev");
+    const next = document.querySelector(".next");
+    let currentSlide = 0;
+    const totalSlides = slides.length;
+    let slideInterval = setInterval(showNextSlide, 5000); // 5 seconds
+  
+    function showSlide(index) {
+      slides.forEach((slide, idx) => {
+        slide.classList.remove("active");
+        if (idx === index) {
+          slide.classList.add("active");
+        }
+      });
+    }
+  
+    function showNextSlide() {
+      currentSlide = (currentSlide + 1) % totalSlides;
+      showSlide(currentSlide);
+    }
+  
+    function showPrevSlide() {
+      currentSlide = (currentSlide - 1 + totalSlides) % totalSlides;
+      showSlide(currentSlide);
+    }
+  
+    next.addEventListener("click", function () {
+      clearInterval(slideInterval);
+      showNextSlide();
+      slideInterval = setInterval(showNextSlide, 5000);
     });
-  }
-
-  function showNextSlide() {
-    currentSlide = (currentSlide + 1) % totalSlides;
-    showSlide(currentSlide);
-  }
-
-  function showPrevSlide() {
-    currentSlide = (currentSlide - 1 + totalSlides) % totalSlides;
-    showSlide(currentSlide);
-  }
-
-  next.addEventListener("click", function () {
-    clearInterval(slideInterval);
-    showNextSlide();
-    slideInterval = setInterval(showNextSlide, 5000);
+  
+    prev.addEventListener("click", function () {
+      clearInterval(slideInterval);
+      showPrevSlide();
+      slideInterval = setInterval(showNextSlide, 5000);
+    });
   });
-
-  prev.addEventListener("click", function () {
-    clearInterval(slideInterval);
-    showPrevSlide();
-    slideInterval = setInterval(showNextSlide, 5000);
-  });
-});
+  
